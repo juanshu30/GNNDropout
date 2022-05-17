8413e2a0765750f2b12ed44a635aa4c64d57d06a
Juan Shu <42724017+juanshu30@users.noreply.github.com>
2022-05-17 10:08:28 -0400

    Update LCD_AHD_Conv.py

--- deep network/cora/LCD_AHD_Conv.py
+++ deep network/cora/LCD_AHD_Conv.py
@@ -172,4 +172,3 @@ for i in np.array([8,16,32,64]):
     log = 'best Epoch: {:03d}, Val loss: {:.4f}, Test acc: {:.4f}'
     print(log.format(best_epoch, best_val_loss, test_acc))
     
-#np.savetxt("/home/shu30/Research_GraphDropout/AAAI/result_deep/GS_cora_ADFF.txt",test_accs)
