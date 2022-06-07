b9259ff61d709fd65ee8dfece4e585c66f351693
Juan Shu <42724017+juanshu30@users.noreply.github.com>
2022-06-07 01:48:22 -0400

    Update FAD_Node.py

--- shallow nework/cora/FAD_Node.py
+++ shallow nework/cora/FAD_Node.py
@@ -126,7 +126,4 @@ for i in np.arange(0.1, 1, 0.1):
         print(test_acc)
     accs_mean.append(np.mean(test_accs))
     accs_var.append(np.var(test_accs))
-    
-    
-np.savetxt("/home/shu30/Research_GraphDropout/AAAI/result/GS_FAD_Node_mean.txt",accs_mean)
-np.savetxt("/home/shu30/Research_GraphDropout/AAAI/result/GS_FAD_Node_var.txt",accs_var)
+   
