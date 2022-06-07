907abb3ff69967cdb14da835d508f142a14bd217
Juan Shu <42724017+juanshu30@users.noreply.github.com>
2022-06-07 01:47:48 -0400

    Update Cora_AHD.py

--- shallow nework/cora/Cora_AHD.py
+++ shallow nework/cora/Cora_AHD.py
@@ -166,6 +166,3 @@ for j in range(2):
 accs_mean.append(np.mean(test_accs))
 accs_var.append(np.var(test_accs))
     
-    
-#np.savetxt("/home/shu30/Research_GraphDropout/AAAI/result/cora_AHD.txt",accs_mean)
-#np.savetxt("/home/shu30/Research_GraphDropout/AAAI/result/cora_AHD_var.txt",accs_var)
